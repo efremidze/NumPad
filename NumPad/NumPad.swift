a6a86ccccd7d00f9ebe5da3008ddb981eafb7221
Lasha Efremidze <efremidzel@hotmail.com>
2016-03-07 13:27:59 -0800

    improvement

--- NumPad/NumPad.swift
+++ NumPad/NumPad.swift
@@ -218,7 +218,7 @@ protocol ReusableView: class {
 
 extension ReusableView where Self: UIView {
     static var defaultReuseIdentifier: String {
-        return NSStringFromClass(self)
+        return String(self)
     }
 }
 
