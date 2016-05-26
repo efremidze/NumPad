b173de3b8030ae494ad6f157f00183346b8394fc
Lasha Efremidze <efremidzel@hotmail.com>
2016-05-26 00:27:40 -0700

    Adding Tests

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import NumPad
 
 class Tests: XCTestCase {
     
@@ -23,13 +24,6 @@ class Tests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
     }
     
 }
